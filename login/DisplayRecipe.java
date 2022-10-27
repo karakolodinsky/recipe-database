66b094e0cbe5376c220a285a459881de01ec5504
Teagan Nester <ten2068@g.rit.edu>
2022-10-27 11:06:41 -0400

    display uneditable recipe

--- login/DisplayRecipe.java
+++ login/DisplayRecipe.java
@@ -2,6 +2,8 @@ package login;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException; 
 
@@ -39,6 +41,7 @@ public class DisplayRecipe extends JFrame {
         JScrollPane scrollPane = new JScrollPane(panel);
 
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setAlignmentY(TOP_ALIGNMENT);
 
         panel.add(Box.createVerticalGlue());
         formatRecipe();
@@ -47,15 +50,79 @@ public class DisplayRecipe extends JFrame {
     }
 
     private void formatRecipe () throws SQLException {
+        Connection con = DataBase.getCon();
         String avg = "";
         String[] info = btnText.split("Avg Rating: ");
         if (info.length > 1) {
             avg = info[1];
         }
-        JButton recipe = new JButton("recipe");
-        panel.add(recipe);
+        JLabel label = new JLabel(info[0]);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(label);
+        if (!(avg.equals(""))) {
+            label = new JLabel("Average Rating: " + avg);
+            label.setAlignmentX(Component.CENTER_ALIGNMENT);
+            panel.add(label);
+        }
+        PreparedStatement ps = con.prepareStatement("SELECT cooktime, steps, description, servings, difficulty, date " +
+                                "FROM recipe WHERE recipeId=?;");
+        ps.setInt(1, recipeId);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int diff = rs.getInt("difficulty");
+        String diffString = difficulty(diff);
+        labelMaker("Difficulty: " + diffString);
+        int time = rs.getInt("cooktime");
+        labelMaker("Cook Time: " + time);
+        int serv = rs.getInt("servings");
+        labelMaker("Servings: " + serv);
+        String desc = rs.getString("description");
+        textArea("Description:\n" + desc);
+        String steps = rs.getString("steps");
+        textArea("Steps: " + steps);
+
 
         validate();
+    }
+
+    private void labelMaker (String text) {
+        JLabel label = new JLabel(text);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(label);
+    }
+
+    private void textArea (String text) {
+        JTextArea textArea = new JTextArea(text);
+        textArea.setEditable(false);  
+        textArea.setCursor(null);  
+        textArea.setOpaque(false);  
+        textArea.setFocusable(false);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setAlignmentX(CENTER_ALIGNMENT);
+        panel.add(textArea);
+        panel.add(textArea);
+    }
+
+    private String difficulty (int diff) {
+        String diffString = "";
+        if (diff == 1) {
+            diffString = "Easy";
+        }
+        else if (diff == 2) {
+            diffString = "Easy-Medium";
+        }
+        else if (diff == 3) {
+            diffString = "Medium";
+        }
+        else if (diff == 4) {
+            diffString = "Medium-Hard";
+        }
+        else {
+            diffString = "Hard";
+        }
+        return diffString;
+            
     }
 
     public static void main(String[] args) {
